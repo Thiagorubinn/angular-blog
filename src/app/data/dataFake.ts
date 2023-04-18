2f5e72e8f122e27f4751b5f07de9f704f91af5b8
Thiago Rubin <rubinthiago@gmail.com>
2023-04-18 15:02:40 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,28 @@
+import { Title } from "@angular/platform-browser";
+
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "A gestão de documentos por integração de sistemas",
+    "description": "ola",
+    "photoCover":"https://blog.assine.online/wp-content/uploads/2020/12/gestao-de-documentos-2-1280x720.jpg"
+  },
+  {
+    "id":"2",
+    "title": "saiu novo conteudo",
+    "description": "Veja como pode ser fácil",
+    "photoCover":"https://factorialhr.com.br/wp-content/uploads/2022/02/18172041/integracao-na-empresa.jpg"
+  },
+  {
+    "id":"3",
+    "title": "AQUI NÃO",
+    "description": "Veja como pode ser fácil",
+    "photoCover":"https://esales.com.br/wp-content/uploads/2019/11/o-que-voce-precisa-saber-sobre-guarda-de-documentos-fiscais-1.jpg"
+  },
+  {
+    "id":"4",
+    "title": "VOLTE",
+    "description": "Veja como pode ser fácil",
+    "photoCover":"https://blog.brasilcloud.com.br/wp-content/uploads/2019/11/217982-guarda-de-documentos-entenda-a-sua-importancia-e-como-fazer-da-maneira-correta.jpg"
+  },
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {dataFake} from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -7,13 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit{
 
-  photoCover:string = "https://www.unodata.com.br/wp-content/uploads/2022/10/tecnologia-1.jpg"
-  contentTitle:string = "NOTÍCIA"
+  photoCover:string = ""
+  contentTitle:string = ""
   contentDescription:string = "HELLO"
+  private id:string | null = "0"
 
-  constructor(){}
+  constructor(
+    private route: ActivatedRoute
+  ){}
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe( value =>
+      this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id)
+  }
+
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+
+
+      this.contentTitle = result.title
+      this.contentDescription = result.description
+      this.photoCover = result.photoCover
 
   }
 
